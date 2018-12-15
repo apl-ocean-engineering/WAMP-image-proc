2a1f8833eae45f7f56ff063cb60863662aab3b1d
Mitchell Scott <miscott@uw.edu>
2018-12-14 17:10:09 -0800

    cleaned

--- scripts/calibrate_images.py
+++ scripts/calibrate_images.py
@@ -1,18 +1,22 @@
-import numpy as np
-import cv2
-import glob
-import matplotlib.pyplot as plt
+#!/usr/bin/env python2.7
+"""
+Created on Wed Nov 14 16:07:34 2018
+
+@author: Mitchell Scott
+@contact: miscott@uw.edu
+"""
 from wampImageProc import calibration
-from types import FunctionType
 
-C = calibration()
-
-image_path = '/home/mitchell/3G-AMP-Calibration/calibration_images'
-base_save_path = '/home/mitchell/WAMP_workspace/WAMP-image-proc/calibration/3G-AMP'
-
-#a = [x for x, y in C.__dict__.items() if type(y) == FunctionType]
-#print(C.__dict__.items())
-
-
-C.stereo_calibrate(image_path + '/Manta 1/2018_12_12_14_19_57.52.jpg', 
-                        image_path + '/Manta 2/2018_12_12_14_19_57.52.jpg', save = True, intrinsics_load_path = base_save_path, stereo_save_path = base_save_path)
+if __name__ == '__main__':
+    image_path = '/home/mitchell/3G-AMP-Calibration/calibration_images'
+    base_save_path = '/home/mitchell/WAMP_workspace/WAMP-image-proc/calibration/3G-AMP'
+    
+    C = calibration()
+    C.intrinsic_calibration(intrinsic_images_path = image_path)
+    '''
+    Stereo calibration
+    '''
+    C.stereo_calibrate(image_path + '/Manta 1/2018_12_12_14_19_57.52.jpg', 
+                            image_path + '/Manta 2/2018_12_12_14_19_57.52.jpg', 
+                            save = True, intrinsics_load_path = base_save_path, 
+                            stereo_save_path = base_save_path)
